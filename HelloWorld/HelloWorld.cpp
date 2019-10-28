0a16d407ff496627bdeb8a2570101f2b6d19177a
Dipod <dipod.dopid@gmail.com>
2019-10-28 16:29:27 +0300

    1.3 Решение задания Парты

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 int main()
 {
-	int Number;
-    std::cin >> Number;
-	std::cout << (Number/2 + 1) * 2;
+	int firstClass, secondClass, thirdClass;
+    std::cin >> firstClass >> secondClass >> thirdClass;
+	std::cout << (firstClass + 1) / 2 + (secondClass + 1) / 2 + (thirdClass + 1) / 2;
 	return 0;
 }
