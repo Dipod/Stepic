b33fff5e4b3d08d15156d0f800df0c56073d7e59
Dipod <dipod.dopid@gmail.com>
2019-10-28 17:20:50 +0300

    1.3 Решение задачи Разность времен

--- HelloWorld/StepicCource.cpp
+++ HelloWorld/StepicCource.cpp
@@ -1,17 +1,9 @@
 ﻿#include <iostream>
-#include <iomanip>
 using namespace std;
 int main()
 {
-	int seconds;
-	cin >> seconds;
-	seconds = seconds % 86400;
-	int hours = seconds / 3600;
-	seconds = seconds % 3600;
-	int minutes = seconds / 60;
-	seconds = seconds % 60;
-	cout << hours << ":";
-	cout.fill('0');
-	cout << setw(2) << minutes << ":" << setw(2) << seconds;
+	int h1, m1, s1, h2, m2, s2;
+	cin >> h1 >> m1 >> s1 >> h2 >> m2 >> s2;
+	cout << (h2 * 60 * 60 + m2 * 60 + s2) - (h1 * 60 * 60 + m1 * 60 + s1);
 	return 0;
 }
