eb6610f4dc341553a8abb053c0f942b8a6524f7e
Dipod <dipod.dopid@gmail.com>
2019-10-29 11:28:59 +0300

    1.4 Решение задания Допустимые ходы слона

--- HelloWorld/StepicCource.cpp
+++ HelloWorld/StepicCource.cpp
@@ -4,7 +4,9 @@ int main()
 {
 	int x1, y1, x2, y2;
 	cin >> x1 >> y1 >> x2 >> y2;
-	if (x2 - x1 <= 1 && x2 - x1 >= -1 && y2 - y1 <= 1 && y2 - y1 >= -1){
+	int x_shift = (x2 - x1) * (x2 - x1);
+	int y_shift = (y2 - y1) * (y2 - y1);
+	if (x_shift == y_shift){
 		cout << "YES";
 	}
 	else {
