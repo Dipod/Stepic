833921a55a3c09f0c430fceb3c6f349c6c216a1d
Dipod <dipod.dopid@gmail.com>
2019-10-30 09:31:56 +0300

    1.4 Решение задачи Деление шоколадки

--- HelloWorld/StepicCource.cpp
+++ HelloWorld/StepicCource.cpp
@@ -1,15 +1,44 @@
-﻿#include <iostream>
+﻿//Шоколадка имеет вид прямоугольника, разделенного на N×M долек.Шоколадку можно один раз разломить по прямой на две части.Определите, можно ли таким образом отломить от шоколадки ровно K долек.
+//Формат входных данных
+//Программа получает на вход три числа : N, M, K
+//Формат выходных данных
+//Программа должна вывести одно из двух слов : YES или NO.
+//
+//Sample Input 1 :
+//
+//	4
+//	2
+//	6
+//
+//Sample Output 1:
+//
+//	YES
+//
+//Sample Input 2 :
+//
+//	2
+//	10
+//	7
+//
+//Sample Output 2:
+//
+//	NO
+//
+//Напишите программу.Тестируется через stdin → stdout
+//Time Limit : 1 секунда
+//Memory Limit : 256 MB
+
+#include <iostream>
 using namespace std;
 int main()
 {
-	int x1, y1, x2, y2;
-	cin >> x1 >> y1 >> x2 >> y2;
-	int x_shift = (x2 - x1) * (x2 - x1);
-	int y_shift = (y2 - y1) * (y2 - y1);
-	if (x_shift == 1 && y_shift == 4 || x_shift == 4 && y_shift == 1){
+	int N, M, K;
+	cin >> N >> M >> K;
+
+	if ((K % N == 0 || K % M == 0) && (K <= N * M)){
 		cout << "YES";
 	}
-	else {
+	else{
 		cout << "NO";
 	}
 	return 0;
