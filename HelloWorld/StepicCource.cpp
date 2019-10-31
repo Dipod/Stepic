3b32a598e248b2fb74a1eda7ec42aad8f37f54e3
Dipod <dipod.dopid@gmail.com>
2019-10-31 18:01:56 +0300

    1.5 Решение задания Вывод квадратов натуральных чисел

--- HelloWorld/StepicCource.cpp
+++ HelloWorld/StepicCource.cpp
@@ -1,89 +1,36 @@
-﻿//Есть две коробки, первая размером A1×B1×C1, вторая размером A2×B2×C2.Определите, можно ли разместить одну из этих коробок внутри другой, при условии, что поворачивать коробки можно только на 90 градусов вокруг ребер.
+﻿//По данному целому числу N распечатайте все квадраты натуральных чисел, не превосходящие N, в порядке возрастания.
 //Формат входных данных
-//Программа получает на вход числа A1, B1, C1, A2, B2, C2.
+//
+//Вводится натуральное число.
 //Формат выходных данных
-//Программа должна вывести одну из следующих строчек :
-//Boxes are equal, если коробки одинаковые,
-//The first box is smaller than the second one, если первая коробка может быть положена во вторую,
-//The first box is larger than the second one, если вторая коробка может быть положена в первую,
-//Boxes are incomparable, во всех остальных случаях.
 //
-//Sample Input 1:
+//Выведите ответ на задачу.
 //
-//	1
-//	2
-//	3
-//	3
-//	2
-//	1
+//Sample Input :
 //
-//Sample Output 1:
+//	50
 //
-//	Boxes are equal
+//Sample Output :
 //
-//Sample Input 2 :
-//
-//	2
-//	2
-//	3
-//	3
-//	2
-//	1
-//
-//Sample Output 2:
-//
-//	The first box is larger than the second one
+//	1 4 9 16 25 36 49
 //
 //Напишите программу.Тестируется через stdin → stdout
 
 #include <iostream>;
 using namespace std;
 
-void sortVariables(int* a, int* b, int* c) {
-	int buffer;
-
-	if (*a > * b) {
-		buffer = *b;
-		*b = *a;
-		*a = buffer;
-	}
-
-	if (*b > * c) {
-		buffer = *c;
-		*c = *b;
-		*b = buffer;
-	}
-
-	if (*a > * b) {
-		buffer = *b;
-		*b = *a;
-		*a = buffer;
-	}
-}
-
 int main()
 {
-	int x1, y1, z1, x2, y2, z2, buffer;
-	cin >> x1 >> y1 >> z1 >> x2 >> y2 >> z2;
-
-	sortVariables(&x1, &y1, &z1);
-	sortVariables(&x2, &y2, &z2);
-
-	if (x1 == x2 && y1 == y2 && z1 == z2) {
-		cout << "Boxes are equal";
-		return 0;
-	}
-
-	if (x1 <= x2 && y1 <= y2 && z1 <= z2) {
-		cout << "The first box is smaller than the second one";
-		return 0;
-	}
+	int N, i = 1;
+	cin >> N;
 	
-	if (x1 >= x2 && y1 >= y2 && z1 >= z2) {
-		cout << "The first box is larger than the second one";
-		return 0;
+	while (i * i <= N) {
+		if (i > 1) {
+			cout << " ";
+		}
+		cout << i * i;
+		i++;
 	}
 
-	cout << "Boxes are incomparable";
 	return 0;
 }
