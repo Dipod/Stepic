e6b26f6e58895b42aa5dad94299bed41d6b6efa4
Dipod <dipod.dopid@gmail.com>
2019-11-02 22:40:53 +0300

    1.6 Решение задачи Вычисление стандартного отклонения

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
-//Определите среднее значение всех элементов последовательности, завершающейся числом 0.
+//Дана последовательность натуральных чисел x1, x2, ..., xn. Определите стандартное отклонение для данной последовательности натуральных чисел, завершающейся числом 0.
 //
 //Формат входных данных
-//Вводится последовательность целых чисел, оканчивающаяся числом 0 (само число 0 в последовательность не входит, а служит как признак ее окончания).
+//Вводится последовательность натуральных чисел, оканчивающаяся числом 0 (само число 0 в последовательность не входит, а служит как признак ее окончания). В последовательности не менее двух чисел до 0.
 //Формат выходных данных
 //Выведите ответ на задачу.
 //
@@ -14,26 +14,28 @@
 //
 //Sample Output:
 //
-//  5.66666666667
+//  4.16333199893
 //
 //Напишите программу. Тестируется через stdin → stdout
 
 #include <iostream>
 #include <iomanip>
+#include <math.h>
 using namespace std;
 
 int main()
 {
-    int N, sum = 0, counter = 0;
-    double result = 0.0;
+    int N, sum = 0, sumOfSquares = 0, counter = 0;
+    double deviation;
     cin >> N;
 
     while (N != 0) {
         sum = sum + N;
+        sumOfSquares = sumOfSquares + N * N;
         counter++;
         cin >> N;
     }
-    result = (double)sum / (double)counter;
-    cout << fixed << setprecision(6) << result;
+    deviation = sqrt((sumOfSquares - (double)sum * (double)sum / (double)counter) / (counter - 1));
+    cout << fixed << setprecision(6) << deviation;
     return 0;
 }
