f210ba8eb0faf572440e8780dc5744baf2b0c6f8
Dipod <dipod.dopid@gmail.com>
2019-11-04 00:55:45 +0300

    1.7 Решение задачи Проверка расстановки 8-ми ферзей

--- main.cpp
+++ main.cpp
@@ -1,27 +1,39 @@
-//Дан массив. Выведите те его элементы, которые встречаются в массиве только один раз. Элементы нужно выводить в том порядке, в котором они встречаются в списке.
+//Известно, что на доске 8×8 можно расставить 8 ферзей так, чтобы они не били друг друга. Вам дана расстановка 8 ферзей на доске, определите, есть ли среди них пара бьющих друг друга.
 //
 //Формат входных данных
-//В первой строке вводится количество элементов в массиве. Во второй строке вводятся элементы массива.
+//Программа получает на вход восемь пар чисел, каждое число от 1 до 8 - координаты 8 ферзей.
 //Формат выходных данных
-//Выведите ответ на задачу.
+//Если ферзи не бьют друг друга, выведите слово NO, иначе выведите YES.
 //
 //Sample Input 1:
 //
-//    6
-//    1 2 2 3 3 3
+//    1 7
+//    2 4
+//    3 2
+//    4 8
+//    5 6
+//    6 1
+//    7 3
+//    8 5
 //
 //Sample Output 1:
 //
-//    1
+//    NO
 //
 //Sample Input 2:
 //
-//    8
-//    4 3 5 2 5 1 3 5
+//    1 8
+//    2 7
+//    3 6
+//    4 5
+//    5 4
+//    6 3
+//    7 2
+//    8 1
 //
 //Sample Output 2:
 //
-//    4 2 1
+//    YES
 //
 //Напишите программу. Тестируется через stdin → stdout
 
@@ -29,32 +41,31 @@
 #include <vector>
 using namespace std;
 
+bool beatCheck(int x1, int y1, int x2, int y2)
+{
+    int x_shift = (x2 - x1) * (x2 - x1);
+    int y_shift = (y2 - y1) * (y2 - y1);
+    return x_shift == y_shift || (x1 == x2 || y1 == y2);
+}
+
 int main()
 {
-    int n;
-    bool unique = true;
-    cin >> n;
-    vector <int> array(n);
-    vector <int> result;
+    vector <int> arrayX(8);
+    vector <int> arrayY(8);
     //array input
-    for(int i = 0; i < n; i++){
-        cin >> array[i];
+    for(int i = 0; i < 8; i++){
+        cin >> arrayX[i];
+        cin >> arrayY[i];
     }
     //array processing
-    for(int j = 0; j < n; j++){
-        for(int i = 0; i < n; i++){
-            if(i != j && array[j] == array[i]){
-                unique = false;
-                break;
+    for(int j = 0; j < 8; j++){
+        for(int i = 0; i < 8; i++){
+            if(i != j && beatCheck(arrayX[j],arrayY[j],arrayX[i],arrayY[i])){
+                cout << "YES";
+                return 0;
             }
         }
-        if(unique){
-            result.push_back(array[j]);
-        }
-        unique = true;
     }
-    //array output
-    for(auto now : result){
-        cout << now << " ";
-    }
+    cout << "NO";
+    return 0;
 }
