71bc851ca2bc6560626404df0587826fb5b653eb
Dipod <dipod.dopid@gmail.com>
2019-11-03 23:41:43 +0300

    1.7 Решение задачи Замена элементов массива попарно

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//Дан список, упорядоченный по неубыванию элементов в нем. Определите, сколько в нем различных элементов.
+//Переставьте соседние элементы массива (A[0] c A[1], A[2] c A[3] и т.д.). Если элементов нечетное число, то последний элемент остается на своем месте.
 //
 //Формат входных данных
 //В первой строке вводится количество элементов в массиве. Во второй строке вводятся элементы массива.
@@ -7,12 +7,12 @@
 //
 //Sample Input:
 //
-//    6
-//    1 2 2 3 3 3
+//    5
+//    1 2 3 4 5
 //
 //Sample Output:
 //
-//    3
+//    2 1 4 3 5
 //
 //Напишите программу. Тестируется через stdin → stdout
 
@@ -25,14 +25,17 @@ int main()
     int n, temp;
     vector <int> array;
     cin >> n;
-    cin >> temp;
-    array.push_back(temp);
-
-    for(int i = 1; i < n; i++){
+    //array input
+    for(int i = 0; i < n; i++){
         cin >> temp;
-        if(array.back() != temp){
-            array.push_back(temp);
-        }
+        array.push_back(temp);
     }
-    cout << array.size();
+    //array processing
+    for(int i = 1; i < n; i += 2){
+        swap(array[i], array[i-1]);
+    }
+    //array output
+    for(auto now : array){
+        cout << now << " ";
+    }
 }
