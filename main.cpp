5acdf18fc252cf1375130b2320a9a03a38258f2f
Dipod <dipod.dopid@gmail.com>
2019-11-05 15:23:39 +0300

    1.9 Решение задачи Вычисление минимума 4-х чисел с помощью функций

--- main.cpp
+++ main.cpp
@@ -1,51 +1,37 @@
-//По данным числам n и m заполните двумерный массив размером n×m числами от 1 до n×m “диагоналями”, как показано в примере.
+//Напишите функцию min(a, b), вычисляющую минимум двух чисел. Затем напишите функцию min4(a, b, c, d), вычисляющую минимум 4 чисел с помощью функции min. Считайте четыре целых числа и выведите их минимум.
 //Формат входных данных
-//
-//Вводятся два числа n и m, не превышающие 100.
+//Вводятся четыре целых числа.
 //Формат выходных данных
-//
-//Выведите полученный массив, отводя на вывод каждого элемента ровно 4 символа.
+//Выведите ответ на задачу.
 //
 //Sample Input:
 //
-//    3 5
+//    4
+//    5
+//    6
+//    7
 //
 //Sample Output:
 //
-//    1   2   4   7  10
-//    3   5   8  11  13
-//    6   9  12  14  15
+//    4
 //
 //Напишите программу. Тестируется через stdin → stdout
 
 #include <iostream>
-#include <iomanip>
 using namespace std;
+
+int min(int a, int b){
+    return a < b ? a : b;
+}
+
+int min4(int a, int b, int c, int d){
+    return min(min(a,b),min(c,d));
+}
 
 int main()
 {
-    int n, m, col = 0, row = 0, counter = 1;
-    cin >> n >> m;
-    int array[n][m];
-    //array input
-    while(counter <= n * m) {
-        if(row >= 0 && row < m) {
-            array[col][row] = counter;
-            counter++;
-        }
-        col++;
-        row--;
-        if(col == n){
-            col = 0;
-            row += n + 1;
-        }
-    }
-    //array output
-    for(col = 0; col < n; col++){
-        for(row = 0; row < m; row++){
-            cout << setw(3) << array[col][row] << ' ';
-        }
-        cout << endl;
-    }
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    cout << min4(a,b,c,d);
     return 0;
 }
