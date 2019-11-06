e1db48ffb70135aa30f87bf114444fe7469e111e
Dipod <dipod.dopid@gmail.com>
2019-11-06 10:02:34 +0300

    1.10 Решение задачи Вывод самого длинного слова строки

--- main.cpp
+++ main.cpp
@@ -1,54 +1,53 @@
-//Проверьте, принадлежит ли точка данной закрашенной области:
+//Найдите в данной строке самое длинное слово и выведите его.
 //
-//Если точка принадлежит области (область включает границы), выведите слово YES, иначе выведите слово NO.
-//Решение должно содержать функцию IsPointInArea(x, y), возвращающую True, если точка принадлежит области и False, если не принадлежит. Основная программа должна считать координаты точки, вызвать функцию IsPointInArea и в зависимости от возвращенного значения вывести на экран необходимое сообщение.
-//Функция IsPointInArea не должна содержать инструкцию if.
-//Формат входных данных
-//Вводятся два действительных числа.
-//Формат выходных данных
-//Выведите ответ на задачу.
+//Входные данные
 //
-//Sample Input 1:
+//Вводится одна строка. Слова в ней отделены одним пробелом.
 //
-//    -4
-//    -4
+//Выходные данные
 //
-//Sample Output 1:
+//Выведите самое длинное слово. Если таких слов несколько, то выводить нужно, которое встречается раньше.
 //
-//    NO
+//Sample Input:
 //
-//Sample Input 2:
+//    Everyone of us has all we need
 //
-//    -4
-//    -3
+//Sample Output:
 //
-//Sample Output 2:
-//
-//    NO
+//    Everyone
 //
 //Напишите программу. Тестируется через stdin → stdout
 
 #include <iostream>
-#include <cmath>
+#include <string>
 using namespace std;
-
-bool IsPointInArea(double x, double y){
-    return (pow(x + 1,2) + pow(y - 1,2) <= 4 &&
-            x + y >= 0 &&
-            y - 2 * x >= 2) ||
-            (pow(x + 1,2) + pow(y - 1,2) >= 4 &&
-            x + y <= 0 &&
-            y - 2 * x <= 2);
-}
 
 int main()
 {
-    double x, y;
-    cin >> x >> y;
-    if(IsPointInArea(x, y)){
-        cout << "YES";
-    }else{
-        cout << "NO";
+    string s, result;
+    int wordLength = 0, wordBeginPos = 0, maxWordLength = 0;
+    getline(cin, s);
+    for(int i = 0; i < s.size(); i++){
+        if (s[i] != ' ') {
+            wordLength++;
+        }else{
+            if(maxWordLength == 0){
+                maxWordLength = wordLength;
+            }else{
+                if (maxWordLength < wordLength){
+                    wordBeginPos = i - wordLength;
+                    maxWordLength = wordLength;
+                }
+            }
+            wordLength = 0;
+        }
     }
+    if (maxWordLength < wordLength){
+        wordBeginPos = s.size() - wordLength;
+        maxWordLength = wordLength + 1;
+    }
+
+    result = s.substr(wordBeginPos,maxWordLength);
+    cout << result;
     return 0;
 }
