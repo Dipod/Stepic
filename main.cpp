880e75e07dea76d75fcec8ec16b6cb47d020dec1
Dipod <dipod.dopid@gmail.com>
2019-11-07 22:00:20 +0300

    1.12 Решение задачи Сортировка участников по набранным баллам и порядковому номеру

--- main.cpp
+++ main.cpp
@@ -1,53 +1,73 @@
-//Даны два списка чисел, которые могут содержать до 100000 чисел каждый.  Выведите все числа, которые входят как в первый, так и во второй список в порядке возрастания.
+//Во время проведения олимпиады каждый из участников получил свой идентификационный номер – натуральное число. Необходимо отсортировать список участников олимпиады по количеству набранных ими баллов.
 //
 //Входные данные
 //
-//Вводится число N - количество элементов первого списка, а затем N чисел первого списка.
-//
-//Затем вводится число M - количество элементов второго списка, а затем M чисел второго списка.
+//На первой строке дано число N (1 ≤ N ≤ 1000) – количество участников. На каждой следующей строке даны идентификационный номер и набранное число баллов соответствующего участника. Все числа во входном файле не превышают 105.
 //
 //Выходные данные
 //
-//Выведите ответ на задачу.
+//В выходной файл выведите исходный список в порядке убывания баллов. Если у некоторых участников одинаковые баллы, то их между собой нужно упорядочить в порядке возрастания идентификационного номера.
 //
-//Sample Input:
+//Sample Input 1:
 //
 //    3
-//    1 3 2
+//    101 80
+//    305 90
+//    200 14
+//
+//Sample Output 1:
+//
+//    305 90
+//    101 80
+//    200 14
+//
+//Sample Input 2:
+//
 //    3
-//    4 3 2
+//    20 80
+//    30 90
+//    25 90
 //
-//Sample Output:
+//Sample Output 2:
 //
-//    2 3
+//    25 90
+//    30 90
+//    20 80
 //
 //Напишите программу. Тестируется через stdin → stdout
 
 #include <iostream>
-#include <set>
 #include <algorithm>
+#include <vector>
+
+struct participant{
+    int indexNumber = 0;
+    int points = 0;
+};
+
+bool participantComparator(const participant a1, const participant a2){
+    if(a1.points == a2.points){
+        return a1.indexNumber < a2.indexNumber;
+    }
+    return a1.points > a2.points;
+};
 
 using namespace std;
 int main()
 {
-    set <int> s1, s2, intersect;
     int n, temp = 0;
     cin >> n;
-    for(int i = 0; i < n; i ++){
-        cin >> temp;
-        s1.insert(temp);
-    }
-    cin >> n;
-    for(int i = 0; i < n; i ++){
-        cin >> temp;
-        s2.insert(temp);
+    vector <participant> participants(n);
+
+    for(int i = 0; i < n; i++){
+        cin >> participants[i].indexNumber;
+        cin >> participants[i].points;
     }
 
-    set_intersection(s1.begin(),s1.end(),s2.begin(),s2.end(),
-                     inserter(intersect,intersect.begin()));
+    sort(participants.begin(),participants.end(),participantComparator);
 
-    for(auto now : intersect){
-        cout << now << " ";
+    for(auto now : participants){
+        cout << now.indexNumber << " " << now.points << endl;
     }
     return 0;
 }
