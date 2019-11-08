9e72b9f28f5b55cb856c0e8f1e0d7515409fa62d
Dipod <dipod.dopid@gmail.com>
2019-11-08 21:48:14 +0300

    1.11 Решение задачи Поиск синонимов в словаре

--- main.cpp
+++ main.cpp
@@ -1,65 +1,47 @@
-//Выведите фамилии и имена учащихся в порядке убывания их среднего балла.
+//Вам дан словарь, состоящий из пар слов. Каждое слово является синонимом к парному ему слову. Все слова в словаре различны. Для одного данного слова определите его синоним.
 //
 //Входные данные
 //
-//Заданы сначала количество учащихся n, затем n строк, каждая из которых содержит фамилию, имя и три числа (оценки по трем предметам: математике, физике, информатике). Данные в строке разделены одним пробелом. Оценки принимают значение от 1 до 5.
+//Программа получает на вход количество пар синонимов N. Далее следует N строк, каждая строка содержит ровно два слова-синонима. После этого следует одно слово.
 //
 //Выходные данные
 //
-//Необходимо вывести пары фамилия-имя по одной на строке, разделяя фамилию и имя одним пробелом. Выводить оценки не нужно. Если несколько учащихся имеют одинаковые средние баллы, то их нужно выводить в порядке, заданном во входных данных.
+//Программа должна вывести синоним к данному слову.
 //
 //Sample Input:
 //
 //    3
-//    Markov Valeriy 5 5 5
-//    Sergey Petrov 1 1 1
-//    Petrov Petr 3 3 3
+//    Hello Hi
+//    Bye Goodbye
+//    List Array
+//    Goodbye
 //
 //Sample Output:
 //
-//    Markov Valeriy
-//    Petrov Petr
-//    Sergey Petrov
+//    Bye
 //
 //Напишите программу. Тестируется через stdin → stdout
 
 #include <iostream>
-#include <algorithm>
-#include <vector>
+#include <map>
 #include <string>
 
 using namespace std;
 
-struct student{
-    string lastName = "";
-    string firstName = "";
-    int mathPoints = 0;
-    int physicsPoints = 0;
-    int compSciencePoints = 0;
-};
-
-bool studentComparator(const student a1, const student a2){
-    return a1.mathPoints + a1.physicsPoints + a1.compSciencePoints > a2.mathPoints + a2.physicsPoints + a2.compSciencePoints;
-}
-
 int main()
 {
-    int n;
+    int n = 0;
     cin >> n;
-    vector <student> students(n);
+    map <string,string> dictionary;
+    string word1, word2;
 
     for(int i = 0; i < n; i++){
-        cin >> students[i].lastName;
-        cin >> students[i].firstName;
-        cin >> students[i].mathPoints;
-        cin >> students[i].physicsPoints;
-        cin >> students[i].compSciencePoints;
+        cin >> word1 >> word2;
+        dictionary[word1] = word2;
+        dictionary[word2] = word1;
     }
 
-    stable_sort(students.begin(),students.end(),studentComparator);
-
-    for(auto now : students){
-        cout << now.lastName << " " << now.firstName << endl;
-    }
+    cin >> word1;
+    cout << dictionary[word1];
     return 0;
 }
