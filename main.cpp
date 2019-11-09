fe60c2aab158cc7d6476a10b13eea9fa21a6f9c4
Dipod <dipod.dopid@gmail.com>
2019-11-09 21:41:01 +0300

    1.9 Решение задачи Размещение N ферзей на доске размером N×N

--- main.cpp
+++ main.cpp
@@ -1,47 +1,74 @@
-//В обувном магазине продается обувь разного размера. Известно, что одну пару обуви можно надеть на другую, если она хотя бы на три размера больше. В магазин пришел покупатель. Требуется определить, какое наибольшее количество пар обуви сможет предложить ему продавец так, чтобы он смог надеть их все одновременно.
+//Дано число N. Определите, сколькими способами можно расставить на доске N×N N ферзей, не бьющих друг друга.
+//Формат входных данных
 //
-//Входные данные
-//
-//Сначала вводится размер ноги покупателя (обувь меньшего размера он надеть не сможет), затем количество пар обуви в магазине и размер каждой пары. Размер — натуральное число, не превосходящее 100, количество пар обуви в магазине не превосходит 1000.
-//
-//Выходные данные
-//
-//Выведите единственное число — максимальное количество пар обуви.
+//Задано единственное число N. (N ≤ 10)
+//Формат выходных данных
+//Выведите ответ на задачу.
+//Подсказка
+//Напишите рекурсивную функцию, которая пытается поставить ферзя в очередной столбец. Если на эту клетку ставить ферзя нельзя (он бьет предыдущих), то такой вариант даже не стоит рассматривать. Когда вы успешно поставили ферзя в последний столбец - увеличивайте счетчик.
 //
 //Sample Input:
 //
-//    26
-//    5
-//    30 35 40 41 42
+//    8
 //
 //Sample Output:
 //
-//    3
+//    92
 //
 //Напишите программу. Тестируется через stdin → stdout
 
 #include <iostream>
-#include <set>
+#include <vector>
 
 using namespace std;
 
-int main()
+struct queenCoordinate{
+    int x = 0;
+    int y = 0;
+};
+
+bool beatCheck(queenCoordinate a1, queenCoordinate a2)
 {
-    int n = 0, temp = 0, footSize = 0, result = 0;
-    cin >> footSize >> n;
-    set <int> shoes;
+    int x_shift = (a2.x - a1.x) * (a2.x - a1.x);
+    int y_shift = (a2.y - a1.y) * (a2.y - a1.y);
+    return x_shift == y_shift || (a1.x == a2.x || a1.y == a2.y);
+}
 
-    for(int i = 0; i < n; i++){
-        cin >> temp;
-        shoes.insert(temp);
-    }
-
-    for(auto now : shoes){
-        if(now >= footSize){
-            result++;
-            footSize = now + 3;
+int solve(vector <queenCoordinate> queenCoordinates, int n, int currentRow){
+    int result = 0;
+    queenCoordinate temp;
+    temp.x = currentRow;
+    for(int col = 1; col <= n; col++){
+        bool badAccommodation = false;
+        temp.y = col;
+        for(auto now : queenCoordinates){
+            if(!badAccommodation){
+                badAccommodation = beatCheck(now,temp);
+            }else{
+                break;
+            }
+        }
+        if(!badAccommodation){
+            if(currentRow == n){
+                result++;
+            }else{
+                vector <queenCoordinate> childQueenCoordinates;
+                childQueenCoordinates = queenCoordinates;
+                childQueenCoordinates.push_back(temp);
+                result += solve(childQueenCoordinates, n, currentRow + 1);
+            }
         }
     }
-    cout << result;
+    return result;
+}
+
+int main()
+{
+    int n = 0;
+    cin >> n;
+    vector <queenCoordinate> queenCoordinates;
+
+    cout << solve(queenCoordinates, n, 1);
+
     return 0;
 }
